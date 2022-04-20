a6e0d2cf49c4bbf2ffed370f1d46dc5c93ce3238
Kfir Back <kfirback03@gmail.com>
2022-04-20 13:16:50 +0300

    HW3

--- HW/Main.c
+++ HW/Main.c
@@ -1,275 +1,53 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#define SIZE 4
+#define N 100
+#define SIZE 10
+#define FACTORIAL 362880
+#define SUMNINE 45
+
+//2
+int IsValidRoute(int arr[],int sum,int atzeret, int n){// sum saves the sum of all the indexes we passed through n is the index we are in currently, , atzeret is the muliplication of all location we have been in
+    if(sum == SUMNINE && atzeret == FACTORIAL){
+        return n == 0;//the sum of  0 + 1 +  2 + 3 + 4 + 5 + 6 + 7 + 8 + 9 is 45 so a valid route will through all of them once and the sum has to be 45,the factorail of 9 is 362880 that means that atzeret has to be equal to that so that every house is contained, n==0 is checking if we are where we started
+    }
+    if(sum > SUMNINE){//if the sum is bigger than 45 then the route has to be invalid
+        return 0;
+    }
+    return IsValidRoute(arr,sum + n,(n)?atzeret*n:atzeret, arr[n]);//going to the next "house" and adding to the sum,does not multiply atzeret if 0
+}
+//1
+void reverseCode(char arr[],int i)
+{
+    if(arr[i] == '\n' || arr[i] == '\0'){//return if its the last character of the string
+        return;
+    }
+    reverseCode(arr,i + 1);//call for  next character
+    if((arr[i] >= 'a' && arr[i] <= 'z') || (arr[i] >= 'A' && arr[i] <= 'Z' ) || (arr[i] >= '0' && arr[i] <= '9'))//check if its in the Alphabet or 0-9
+    {
+        printf("%c", arr[i]);//print the letter if so
+        return;
+    }
+}
 
 int main()
 {
-    float browniePrice , cheesecakePrice , teaPrice , cafePrice;
-    int i = 0;
-    int customer1[SIZE],customer2[SIZE],customer3[SIZE],customer4[SIZE],customer5[SIZE];//place 0 is for brownies amount 1 is for cheescake amount 2 is for tea amount and 3 is for cafe amount
-    int mostOfThisID = 0, mostOfThisAmount = -1;
-    float sums[SIZE] = {0};// saves the total amount of stuff in sold, seperated in brackets(place 0 is for brownies amount 1 is for cheescake amount 2 is for tea amount and 3 is for cafe amount)
-    float Revenues = 0;
-
-    printf("Welcome to 'Programming and eat' restaurant!\n\n");
-    {//1
-        printf("Enter the brownies price: ");
-        while(scanf("%f",  &browniePrice) == 0 || browniePrice < 0){// input check
-            if (browniePrice < 0){
-                printf("\nEnter the brownies price: ");
-            }
-            else{//in here if error isnt caused by negative number
-                printf("\nInput error!");
-                return 1;
-            }
-        }
-        printf("\nEnter the cheesecake price: ");
-        while(scanf("%f",  &cheesecakePrice) != 1 || cheesecakePrice < 0){// input check
-            if (cheesecakePrice < 0){
-                printf("\nEnter the cheesecake price: ");
-                continue;
-            }
-            else{//in here if error isnt caused by negative number
-                printf("\nInput error!");
-                return 1;
-            }
-        }
-        printf("\nEnter the tea price: ");
-        while(scanf("%f",  &teaPrice) != 1 || teaPrice < 0){//input check
-            if (teaPrice < 0){
-                printf("\nEnter the tea price: ");
-                continue;
-            }
-            else{//in here if error isnt caused by negative number
-                printf("\nInput error!");
-                return 1;
-            }
-        }
-        printf("\nEnter the cafe price: ");
-        while(scanf("%f",  &cafePrice) != 1 || cafePrice < 0){//input check
-            if (cafePrice < 0){
-                printf("\nEnter the cafe price: ");
-                continue;
-            }
-            else{//in here if error isnt caused by negative number
-                printf("\nInput error!");
-                return 1;
-            }
-        }
-    }
-    {// 2 and 3
-        {// Customer 1
-            printf("\nCUSTOMER 1:\n");
-            printf("Enter the brownies amount: ");
-            scanf("%d",  &customer1[0]);
-            if (customer1[0] < 0)//error check
-            {
-                printf("\nInput error!");
-                return 1;
-            }
-            printf("\nEnter the cheesecake amount: ");
-            scanf("%d", &customer1[1]);
-            if(customer1[1] < 0)//error check
-            {
-                printf("\nInput error!");
-                return 1;
-            }
-            printf("\nEnter the tea amount: ");
-            scanf("%d", &customer1[2]);
-            if(customer1[2] < 0)//error check
-            {
-                printf("\nInput error!");
-                return 1;
-            }
-            printf("\nEnter the cafe amount: ");
-            scanf("%d", &customer1[3]);
-            if(customer1[3] < 0)//error check
-            {
-                printf("\nInput error!");
-                return 1;
-            }
-        }
-        {// Customer 2
-            printf("\nCUSTOMER 2:\n");
-            printf("Enter the brownies amount: ");
-            scanf("%d",  &customer2[0]);
-            if (customer2[0] < 0)//error check
-            {
-                printf("\nInput error!");
-                return 1;
-            }
-            printf("\nEnter the cheesecake amount: ");
-            scanf("%d", &customer2[1]);
-            if(customer2[1] < 0)//error check
-            {
-                printf("\nInput error!");
-                return 1;
-            }
-            printf("\nEnter the tea amount: ");
-            scanf("%d", &customer2[2]);
-            if(customer2[2] < 0)//error check
-            {
-                printf("\nInput error!");
-                return 1;
-            }
-            printf("\nEnter the cafe amount: ");
-            scanf("%d", &customer2[3]);
-            if(customer2[3] < 0)//error check
-            {
-                printf("\nInput error!");
-                return 1;
-            }
-        }
-        {// Customer 3
-            printf("\nCUSTOMER 3:\n");
-            printf("Enter the brownies amount: ");
-            scanf("%d",  &customer3[0]);
-            if (customer3[0] < 0)//error check
-            {
-                printf("\nInput error!");
-                return 1;
-            }
-            printf("\nEnter the cheesecake amount: ");
-            scanf("%d", &customer3[1]);
-            if(customer3[1] < 0)//error check
-            {
-                printf("\nInput error!");
-                return 1;
-            }
-            printf("\nEnter the tea amount: ");
-            scanf("%d", &customer3[2]);
-            if(customer3[2] < 0)//error check
-            {
-                printf("\nInput error!");
-                return 1;
-            }
-            printf("\nEnter the cafe amount: ");
-            scanf("%d", &customer3[3]);
-            if(customer3[3] < 0)//error check
-            {
-                printf("\nInput error!");
-                return 1;
-            }
-        }
-        {// Customer 4
-            printf("\nCUSTOMER 4:\n");
-            printf("Enter the brownies amount: ");
-            scanf("%d",  &customer4[0]);
-            if (customer4[0] < 0)//error check
-            {
-                printf("\nInput error!");
-                return 1;
-            }
-            printf("\nEnter the cheesecake amount: ");
-            scanf("%d", &customer4[1]);
-            if(customer4[1] < 0)//error check
-            {
-                printf("\nInput error!");
-                return 1;
-            }
-            printf("\nEnter the tea amount: ");
-            scanf("%d", &customer4[2]);
-            if(customer4[2] < 0)//error check
-            {
-                printf("\nInput error!");
-                return 1;
-            }
-            printf("\nEnter the cafe amount: ");
-            scanf("%d", &customer4[3]);
-            if(customer4[3] < 0)//error check
-            {
-                printf("\nInput error!");
-                return 1;
-            }
-        }
-        {// Customer 5
-            printf("\nCUSTOMER 5:\n");
-            printf("Enter the brownies amount: ");
-            scanf("%d",  &customer5[0]);
-            if (customer5[0] < 0)//error check
-            {
-                printf("\nInput error!");
-                return 1;
-            }
-            printf("\nEnter the cheesecake amount: ");
-            scanf("%d", &customer5[1]);
-            if(customer5[1] < 0)//error check
-            {
-                printf("\nInput error!");
-                return 1;
-            }
-            printf("\nEnter the tea amount: ");
-            scanf("%d", &customer5[2]);
-            if(customer5[2] < 0)//error check
-            {
-                printf("\nInput error!");
-                return 1;
-            }
-            printf("\nEnter the cafe amount: ");
-            scanf("%d", &customer5[3]);
-            if(customer5[3] < 0)//error check
-            {
-                printf("\nInput error!");
-                return 1;
-            }
-        }
-    }
+    char message[N];
+    int route[SIZE];
+    printf("Please enter your message: \n");
+    scanf("%s", message);
+    reverseCode(message, 0);
+    printf("\nPlease enter Daniel route: \n");
+    if(scanf("%d %d %d %d %d %d %d %d %d %d", &route[0] , &route[1], &route[2] , &route[3], &route[4] , &route[5], &route[6] , &route[7], &route[8] , &route[9])!= 10)
     {
-        //4
-        for(i = 0; i < SIZE; i++){//4abcd
-            if(customer1[i] > mostOfThisAmount){//checks for max in each customer in each category and saves it only for once and forgets at the end
-                mostOfThisAmount = customer1[i];
-                mostOfThisID = 1;
-            }
-            if(customer2[i] > mostOfThisAmount){
-                mostOfThisAmount = customer2[i];
-                mostOfThisID = 2;
-            }
-            if(customer3[i] > mostOfThisAmount){
-                mostOfThisAmount = customer3[i];
-                mostOfThisID = 3;
-            }
-            if(customer4[i] > mostOfThisAmount){
-                mostOfThisAmount = customer4[i];
-                mostOfThisID = 4;
-            }
-            if(customer5[i] > mostOfThisAmount){
-                mostOfThisAmount = customer5[i];
-                mostOfThisID = 5;
-            }
-            switch (i)
-            {
-                case 0:
-                    printf("\nThe customer who order the highest amount of brownies is: %d.\n", mostOfThisID);
-                    break;
-                case 1:
-                    printf("The customer who order the highest amount of cheesecakeArr is: %d.\n", mostOfThisID);
-                    break;
-
-                case 2:
-                    printf("The customer who order the highest amount of tea is: %d.\n", mostOfThisID);
-                    break;
-                case 3:
-                    printf("The customer who order the highest amount of cafe is: %d.\n", mostOfThisID);
-                    break;
-            }
-            mostOfThisAmount = -1;
-            mostOfThisID = 0;
-        }
-
-        for(i = 0; i < SIZE; i++){//filling sums with the total orders
-            sums[i] = customer1[i] + customer2[i] + customer3[i] + customer4[i] + customer5[i];
-        }
-        // 4e
-        Revenues = sums[0]*browniePrice + sums[1]*cheesecakePrice + sums[2]*teaPrice + sums[3]*cafePrice;//calculating the revenue
-        printf("The restaurant revenue is: %.2f\n", Revenues);
-
-        //4f
-        printf("The average amount of brownies ordering is: %.2f\n", sums[0]/5);
-        printf("The average amount of cheesecake ordering is: %.2f\n", sums[1]/5);
-        printf("The average amount of tea ordering is: %.2f\n", sums[2]/5);
-        printf("The average amount of cafe ordering is: %.2f\n", sums[3]/5);
+        printf("Invalid Input");
+        return 1;
+    }
+    if(IsValidRoute(route,0,1,0)){
+        printf("I'm coming!\n");
+    }
+    else{
+        printf("Sorry, I did not find a way\n");
     }
     return 0;
 }
